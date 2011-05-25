90e73d0175738ee5fb34cbf70468183b9c427391
Ivo Houbrechts <ivo@houbrechts-it.be>
2011-05-25 14:00:12 +0200

    added plugin info

--- GspTaglibGrailsPlugin.groovy
+++ GspTaglibGrailsPlugin.groovy
@@ -2,13 +2,10 @@ import org.codehaus.groovy.grails.web.pages.GspTagInfo
 import org.codehaus.groovy.grails.web.pages.GspTagParser
 
 class GspTaglibGrailsPlugin {
-    // the plugin version
     def version = "0.1"
-    // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.2.1 > *"
-    // the other plugins this plugin depends on
     def dependsOn = [:]
-    // resources that are excluded from plugin packaging
+
     def pluginExcludes = [
             "grails-app/views/error.gsp"
     ]
@@ -16,11 +13,17 @@ class GspTaglibGrailsPlugin {
     // TODO Fill in these fields
     def author = "Ivo Houbrechts"
     def authorEmail = "ivo@houbrechts-it.be"
-    def title = "GSP tags"
-    def description = "Grails plugin that makes it possible to define tags in GSP's in the grails-app/taglib"
+    def title = "GSP tags plugin"
+    def description = '''\
+Grails plugin that makes it possible to define tags in GSP's in the grails-app/taglib
+'''
 
-    // URL to the plugin's documentation
-    def documentation = "http://grails.org/plugin/gsp-taglib"
+    def license = "APACHE"
+    def organization = [ name: "Ixor", url: "http://www.ixor.be" ]
+    def developers = [ [ name: "Ivo Houbrechts", email: "ivo@houbrechts-it.be" ] ]
+    def scm = [ url: "https://houbie@github.com/houbie/gsp-taglib.git" ]
+
+    def documentation = "http://grails-plugins.github.com/grails-release/"
 
     def watchedResources = ["file:./plugins/*/grails-app/taglib/**/*.gsp",
             "file:./grails-app/taglib/**/*.gsp"]
